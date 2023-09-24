c666c226c68ba22ab4fc40113d120ede3d14df9b
Gautam Raj <ashragautam25@gmail.com>
2023-09-24 22:10:28 +0530

    Update route
    - Add ably auth, message route.

--- routes/ably.js
+++ routes/ably.js
@@ -1,45 +1,93 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const router = express.Router();
+const Ably = require('ably');
 
 require('dotenv').config();
 
 const apiKey = process.env.ABLY_API_KEY;
-const [keyid, keySecret] = apiKey.split(':');
+console.log(apiKey);
+// const [keyid, keySecret] = apiKey.split(':');
 
-const expiresIn = 3600;
-const capability = JSON.stringify({ '*': ['publish', 'subscribe'] });
-const jwtOptions = { expiresIn, keyid };
+// const expiresIn = 3600;
+// const capability = JSON.stringify({ '*': ['publish', 'subscribe'] });
+// const jwtOptions = { expiresIn, keyid };
+// console.log(jwtOptions)
 
-router.get('/getJWT', (req, res) => {
+router.get('/auth', (req, res) => {
   console.log('Sucessfully connected to the server auth endpoint');
 
   // Check user is authorized to access this endpoint
 
-  const clientId = req.query.clientId;
+  const clientId = req.query.id;
 
-  const jwtPayload = {
-    'x-ably-capability': capability,
-    'x-ably-clientId': clientId,
-  };
+  // const jwtPayload = {
+  //   'x-ably-capability': capability,
+  //   'x-ably-clientId': clientId,
+  // };
 
-  jwt.sign(jwtPayload, keySecret, jwtOptions, (err, tokenId) => {
-    console.log('✓ JSON Web Token signed by auth server');
+  // jwt.sign(jwtPayload, keySecret, jwtOptions, (err, tokenId) => {
+  //   console.log('✓ JSON Web Token signed by auth server');
 
-    if (err) return console.error();
+  //   if (err) return console.error();
 
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.setHeader('Content-Type', 'application/json');
+  //   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  //   res.setHeader('Content-Type', 'application/json');
 
-    console.log('Sending signed JWT token back to client');
+  //   res.send(JSON.stringify(tokenId));
+  // });
 
-    res.send(JSON.stringify(tokenId));
-  });
+  try {
+    const client = new Ably.Rest({
+      key: apiKey,
+      prefixUrl: 'https://rest.ably.io',
+    });
+
+    client.auth.createTokenRequest(
+      {
+        clientId,
+      },
+      (err, tokenRequest) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send('Internal Error');
+        }
+
+        console.log('✓ Token request created');
+        console.log(tokenRequest);
+        return res.send(tokenRequest);
+      }
+    );
+  } catch (error) {
+    return res.status(500).send('Internal Error');
+  }
 });
 
 router.get('/logout', (req, res) => {
-  ably.close();
+  Ably.close();
   console.log('Closed the connection to Ably.');
+});
+
+router.post('/message', async (req, res) => {
+  const body = req.body;
+  const client = new Ably.Rest(apiKey);
+  const { message, channelName } = body;
+
+  const channel = client.channels.get(channelName);
+
+  const disallowedWords = ['nigga', 'nude', 'sex'];
+
+  const containsDisallowedWord = disallowedWords.some((word) => {
+    return message.text.match(new RegExp(`\\b${word}\\b`));
+  });
+
+  if (containsDisallowedWord) {
+    return res.status(403).send('Bad word');
+  }
+
+  const response = await channel.publish('update-from-server', message);
+
+  return res.send(response);
 });
 
 module.exports = router;
