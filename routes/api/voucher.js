9c8653cbfc2eb8efcc2a2270717f453dccb4434a
Gautam Raj <ashragautam25@gmail.com>
2023-10-10 22:06:34 +0530

    Update voucher schema

--- routes/api/voucher.js
+++ routes/api/voucher.js
@@ -1,50 +1,50 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const auth = require("../../middleware/auth");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const auth = require('../../middleware/auth');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
-const User = require("../../models/User");
-const Voucher = require("../../models/Voucher");
+const User = require('../../models/User');
+const Voucher = require('../../models/Voucher');
 
 //Route: POST api/voucher/create
 //Description: Create voucher
 //Access: Private
-router.post("/create", auth, async (req, res) => {
+router.post('/create', auth, async (req, res) => {
   const pubkey = req.pubkey;
 
   // Check for pubkey
   if (!pubkey) {
-    return res.status(400).json({ error: "Please provide a pubkey" });
+    return res.status(400).json({ error: 'Please provide a pubkey' });
   }
 
   const user = await User.findOne({ pubkey });
 
   // Check for user
   if (!user) {
-    return res.status(400).json({ error: "User does not exist" });
+    return res.status(400).json({ error: 'User does not exist' });
   }
 
-  const { voucherId, amount, cardColor } = req.body;
+  const { voucherId, amount, cardColor, message } = req.body;
 
   // Check for voucherId
   if (!voucherId) {
-    return res.status(400).json({ error: "Please provide a voucherId" });
+    return res.status(400).json({ error: 'Please provide a voucherId' });
   }
 
   // Check for voucherAmount
   if (!amount) {
-    return res.status(400).json({ error: "Please provide a voucherAmount" });
+    return res.status(400).json({ error: 'Please provide a voucherAmount' });
   }
 
   // Check for cardColor
   if (!cardColor) {
-    return res.status(400).json({ error: "Please provide a cardColor" });
+    return res.status(400).json({ error: 'Please provide a cardColor' });
   }
 
   try {
     if (await Voucher.findOne({ voucherId })) {
-      return res.status(400).json({ error: "Voucher already exists" });
+      return res.status(400).json({ error: 'Voucher already exists' });
     }
 
     const newVoucher = new Voucher({
@@ -53,6 +53,7 @@ router.post("/create", auth, async (req, res) => {
       amount,
       cardColor,
       isClaimed: false,
+      message,
     });
 
     const voucher = await newVoucher.save();
@@ -60,19 +61,19 @@ router.post("/create", auth, async (req, res) => {
     res.json(voucher);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
 });
 
 //Route: GET api/voucher/get
 //Description: Get vouchers
 //Access: Private
-router.get("/get", auth, async (req, res) => {
+router.get('/get', auth, async (req, res) => {
   const pubkey = req.pubkey;
 
   // Check for pubkey
   if (!pubkey) {
-    return res.status(400).json({ error: "Please provide a pubkey" });
+    return res.status(400).json({ error: 'Please provide a pubkey' });
   }
 
   try {
@@ -81,62 +82,62 @@ router.get("/get", auth, async (req, res) => {
     res.json(vouchers);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
 });
 
 //Route: GET api/voucher/get/:id
 //Description: Get voucher
 //Access: Public
-router.get("/get/:id", async (req, res) => {
+router.get('/get/:id', async (req, res) => {
   const voucherId = req.params.id;
 
   // Check for voucherId
   if (!voucherId) {
-    return res.status(400).json({ error: "Please provide a voucherId" });
+    return res.status(400).json({ error: 'Please provide a voucherId' });
   }
 
   try {
     const voucher = await Voucher.findOne({ voucherId });
 
     if (!voucher) {
-      return res.status(400).json({ error: "Voucher does not exist" });
+      return res.status(400).json({ error: 'Voucher does not exist' });
     }
 
     res.json(voucher);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
 });
 
 //Route: POST api/voucher/claim/:id
 //Description: Claim voucher
 //Access: Private
-router.post("/claim/:id", auth, async (req, res) => {
+router.post('/claim/:id', auth, async (req, res) => {
   const pubkey = req.pubkey;
 
   // Check for pubkey
   if (!pubkey) {
-    return res.status(400).json({ error: "Please provide a pubkey" });
+    return res.status(400).json({ error: 'Please provide a pubkey' });
   }
 
   const voucherId = req.params.id;
 
   // Check for voucherId
   if (!voucherId) {
-    return res.status(400).json({ error: "Please provide a voucherId" });
+    return res.status(400).json({ error: 'Please provide a voucherId' });
   }
 
   try {
     const voucher = await Voucher.findOne({ voucherId });
 
     if (!voucher) {
-      return res.status(400).json({ error: "Voucher does not exist" });
+      return res.status(400).json({ error: 'Voucher does not exist' });
     }
 
     if (voucher.isClaimed) {
-      return res.status(400).json({ error: "Voucher already claimed" });
+      return res.status(400).json({ error: 'Voucher already claimed' });
     }
 
     voucher.isClaimed = true;
@@ -146,7 +147,7 @@ router.post("/claim/:id", auth, async (req, res) => {
     res.json(updatedVoucher);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
 });
 
